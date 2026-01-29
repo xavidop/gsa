b59b69dfb8f46cb9b39c9eee6dfaf09f7f08c940
Xavier Portilla Edo <xavierportillaedo@gmail.com>
2026-01-29 14:47:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4970575221-858b3",
+  "appId": "1:279785791414:web:81ed17a035aa856f5d2b17",
+  "apiKey": "AIzaSyBWr4Dk2P_JMtDficykNkgFfYCivYweWhc",
+  "authDomain": "studio-4970575221-858b3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "279785791414"
+};
